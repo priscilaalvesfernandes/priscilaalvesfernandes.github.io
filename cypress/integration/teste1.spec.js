6dbd35f5727b8b1071aea618ea171df22e22663e
priscilaalvesfernandes <priscila_eld@hotmail.com>
2022-02-20 07:23:39 -0400

    Initial commit

---
+++ cypress/integration/teste1.spec.js
@@ -0,0 +1,25 @@
+// teste1.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+
+describe('Meu primeira visita', () => {
+    it('visita site', () => {
+        cy.wait(600)
+        cy.visit('https://priscilaalvesfernandes.github.io')
+    })
+})
+
+describe('Meu primeiro teste', () => {
+    it('teste clique', () => {
+        cy.get("#1").click()
+        cy.get("#2").click()
+        cy.get("#3").click()
+        cy.get("#4").click()
+        cy.get("#5").click()
+
+    })
+})
