9b06db7a9d02adb166d1dcb088abc493f1eeff1d
priscilaalvesfernandes <priscila_eld@hotmail.com>
2022-02-22 08:58:04 -0400

    update2

--- cypress/integration/teste1.spec.js
+++ cypress/integration/teste1.spec.js
@@ -16,19 +16,23 @@ describe('Meu primeira visita', () => {
 describe('Meu primeiro teste', () => {
     it('teste clique', () => {
 
-    /* Testantando botões
+     Testantando botões
         cy.get("#1").click()
         cy.get("#2").click()
         cy.get("#3").click()
         cy.get("#4").click()
         cy.get("#5").click()
-     */
+        cy.get("#6").click()
+        cy.get("#7").click()
+        cy.get("#8").click()
+        cy.get("#9").click()
+     
     })
 })
 
-describe('Repetições', () =>{
+/*describe('Repetições', () =>{
     it ('loop',()=>{
-        
+
         i=="#id"
         for (i=1; i<=9; i++){
 
@@ -40,4 +44,4 @@ describe('Repetições', () =>{
 
 
 
-)
+)*/
