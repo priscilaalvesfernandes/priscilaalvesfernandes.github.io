5ffc0293afeba6051aee4492045ab337c9077413
priscilaalvesfernandes <priscila_eld@hotmail.com>
2022-02-22 13:29:34 -0400

    upd

--- cypress/integration/teste1.spec.js
+++ cypress/integration/teste1.spec.js
@@ -1,45 +1,26 @@
 // teste1.spec.js created with Cypress
 //
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+// .click({force:true}), usado qdo exite hyperlink para um site externo, finalidade de fazer com que o bot ignore mensagens
 
 
-describe('Home', () => {
 
-    it('visita site', () => {
+context("Home", ()=>{
+    beforeEach( ()=>{
         cy.wait(600)
-        cy.visit('https://priscilaalvesfernandes.github.io')//visitando um site
+        cy.visit("https://priscilaalvesfernandes.github.io")
     })
+    var i
+    for(i=0; i<9; i++){
+        it('teste clique',
+            () => {
 
-    it('teste clique',
-        () => {
+                //cy.get("a#") assim chama apena o que esta dentro da tag A.
+                //cy.contains()
+                cy.get("#" +i).click() //adiciona o valor de i ao ID
 
-            //Testantando botões
-            cy.get('#1').click()
-            cy.get('#2').click()
-            cy.get("#3").click()
-            cy.get("#4").click()
-            cy.get("#5").click()
-            cy.get("#6").click()
-
-    })
-})
-
-describe('Visita pagina generica', () => {
-
-    it('visita site', () => {
-        cy.wait(600)
-        cy.visit('https://priscilaalvesfernandes.github.io/generic.html')//visitando um site
-    })
-
-    it('teste clique',
-        () => {
-            cy.get("#7").click()
-            cy.get("#8").click()
-            // cy.get("#9").click()
-            //cy.get("#10").click()
-
+                cy.visit("https://priscilaalvesfernandes.github.io")
         })
+    }
 })
+
+
