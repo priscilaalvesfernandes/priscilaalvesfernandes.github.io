9934d3574be87ab7a0c1af2af133024e153e1baa
priscilaalvesfernandes <priscila_eld@hotmail.com>
2022-02-25 11:42:42 -0400

    upd33 - identação

--- cypress/integration/teste_aleatorio.js
+++ cypress/integration/teste_aleatorio.js
@@ -1,10 +1,5 @@
-// teste_linear.js created with Cypress
-//
-// .click({force:true}), usado qdo exite hyperlink para um site externo, finalidade de fazer com que o bot ignore mensagens
 
-
-
-describe("Teste Linear", ()=>
+describe("Teste aleatório", ()=>
 {
     beforeEach( ()=>
     {
@@ -12,23 +7,23 @@ describe("Teste Linear", ()=>
         cy.visit("https://priscilaalvesfernandes.github.io")
 
     })
-        for (let j=0; j<=12; j++) {
-            let i = Math.ceil(Math.random() * (12 - 1) + 1);
-
-            it("Teste aleatório", () => {
-                if (i > 0 && i < 6) {
-                    cy.visit("https://priscilaalvesfernandes.github.io")
-                    cy.get("#" + i).click({force: true})//criar e chamar um número aleatório
-                }
-                if (i > 5 && i < 11) {
-                    cy.visit("https://priscilaalvesfernandes.github.io/informacoes.html")
-                    cy.get("#" + i).click({force: true})//criar e chamar um número aleatório
-                }
-                if (i == 11 || i == 12) {
-                    cy.visit("https://priscilaalvesfernandes.github.io/trabalho.html")
-                    cy.get("#" + i).click({force: true})//criar e chamar um número aleatório
-                }
-            })
-        }
+    for (let j=0; j<=12; j++) {
+        let i = Math.ceil(Math.random() * (12 - 1) + 1);//aleatorizando númetos de 1-12
+        it("Teste aleatório", () => {
+            if (i > 0 && i < 6) {
+                cy.visit("https://priscilaalvesfernandes.github.io")
+                cy.get("#" + i).click({force: true})//clica no botão que possui o id de acordo com o número sorteado
+            }
+            if (i > 5 && i < 11) {
+                cy.visit("https://priscilaalvesfernandes.github.io/informacoes.html")
+                cy.get("#" + i).click({force: true})//clica no botão que possui o id de acordo com o número sorteado
+            }
+            if (i == 11 || i == 12) {
+                cy.visit("https://priscilaalvesfernandes.github.io/trabalho.html")
+                cy.get("#" + i).click({force: true})//clica no botão que possui o id de acordo com o número sorteado
+            }
+        })
+    }
 })
+// .click({force:true}), usado qdo exite hyperlink para um site externo, finalidade de fazer com que o bot ignore mensagens
 
--- cypress/integration/teste_linear.js
+++ cypress/integration/teste_linear.js
@@ -8,9 +8,8 @@ describe("Teste Linear", ()=>
 {
     beforeEach( ()=>
     {
-        cy.wait(100)
-        cy.visit("https://priscilaalvesfernandes.github.io")
-
+        cy.wait(100)//definindo o tempo de espera em uma página
+        cy.visit("https://priscilaalvesfernandes.github.io")//visitando a página inicial do site
     })
 
     for(let i=1; i<=12; i++)
@@ -18,19 +17,18 @@ describe("Teste Linear", ()=>
         it('teste clique', () =>
         {
            if(i>5 && i<11){
-               cy.visit("https://priscilaalvesfernandes.github.io/informacoes.html")
-
+               cy.visit("https://priscilaalvesfernandes.github.io/informacoes.html")//visitando a página sendo que esteja
+                                                                                    // na página A e precise visitar um id na página B
            }
 
            if (i==11 || i==12){
-               cy.visit("https://priscilaalvesfernandes.github.io/trabalho.html")
+               cy.visit("https://priscilaalvesfernandes.github.io/trabalho.html")//visitando a página sendo que esteja
+                                                                                // na página A e precise visitar um id na página B
            }
            cy.get("#" +i).click() //adiciona o valor de i ao ID
-
             //cy.get("a#") assim chama apena o que esta dentro da tag A. cy.contains()
-
         })
-     }
+    }
 })
 
 
